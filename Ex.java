09d3663115ca36f0e921419a069f73612202b2fb
Vivek Singhal <50330336+viveksinghal32454@users.noreply.github.com>
2022-03-16 10:23:53 +0530

    Add files via upload

---
+++ Ex.java
@@ -0,0 +1,11 @@
+public class Ex {
+    public static void main(String[] args) {
+        System.out.print("Hello \n");//Hello (It is a single line comment)
+        // \n is used for next line
+        System.out.print("Hello1 \n");/**Multiple line comment */
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+
+    }
+    
+}
